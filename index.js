b86f712e18f5b36c9370f9df6107e385f038921f
23birola <23birola@gmail.com>
2024-02-10 19:13:25 +0000

    add function that gather team information

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const render = require("./src/page-template.js");
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
-const questions = [
+const managerQuestions = [
   {
     type: 'input',
     name: 'name',
@@ -39,15 +39,126 @@ const questions = [
     name: 'office number',
     message: 'What is your office number?',
   },
+];
+
+const engineerQuestions = [
+  {
+    type: 'input',
+    name: 'name',
+    message: 'What is your name?',
+  },
+  {
+    type: 'input',
+    name: 'id',
+    message: 'What is your ID?',
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'What is your email?',
+    validate: function (input) {
+      // Use a regular expression for basic email validation
+      const emailRegex = /\S+@\S+\.\S+/;
+      return emailRegex.test(input) ? true : 'Please enter a valid email address';
+    }
+  },
   {
     type: 'input',
     name: 'github',
     message: 'Enter your GitHub Username',
   },
+];
+
+const internQuestions = [
   {
     type: 'input',
-    name: 'linkedin',
-    message: 'Enter your LinkedIn URL.',
+    name: 'name',
+    message: 'What is your name?',
+  },
+  {
+    type: 'input',
+    name: 'id',
+    message: 'What is your ID?',
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'What is your email?',
+    validate: function (input) {
+      // Use a regular expression for basic email validation
+      const emailRegex = /\S+@\S+\.\S+/;
+      return emailRegex.test(input) ? true : 'Please enter a valid email address';
+    }
+  },
+  {
+    type: 'input',
+    name: 'school',
+    message: 'What is your school?',
   },
 ];
 
+const team = [];
+
+function gatherManagerInfo() {
+  inquirer
+    .prompt(managerQuestions)
+    .then((answers) => {
+      const manager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber);
+      team.push(manager);
+      promptUser();
+    });
+}
+
+function gatherEngineerInfo() {
+    inquirer
+    .prompt(engineerQuestions)
+    .then((answers) => {
+      const engineer = new Engineer(answers.name, answers.id, answers.email, answers.github);
+      team.push(engineer);
+      promptUser();
+    });
+}
+
+function gatherInternInfo() {
+   inquirer
+    .prompt(internQuestions)
+    .then((answers) => {
+      const intern = new Engineer(answers.name, answers.id, answers.email, answers.school);
+      team.push(intern);
+      promptUser();
+    });
+}
+
+function promptUser() {
+  inquirer
+    .prompt([
+      {
+        type: 'list',
+        name: 'choice',
+        message: 'What type of team member would you like to add?',
+        choices: ['Engineer', 'Intern', 'Finish building the team'],
+      },
+    ])
+    .then((answers) => {
+      switch (answers.choice) {
+        case 'Engineer':
+          gatherEngineerInfo();
+          break;
+        case 'Intern':
+          gatherInternInfo();
+          break;
+        case 'Finish building the team':
+          generateHTML();
+          break;
+      }
+    });
+}
+
+function generateHTML() {
+  const html = render(team);
+  fs.writeFileSync('./output/team.html', html);
+  console.log('HTML file generated successfully!');
+}
+
+// Start the application by gathering Manager information
+gatherManagerInfo();
