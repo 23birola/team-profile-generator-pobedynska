a84185d5429c0e4162ac997e5ee7292a8c5950b8
23birola <23birola@gmail.com>
2024-02-10 18:50:10 +0000

    created classes

---
+++ index.js
@@ -0,0 +1,53 @@
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const inquirer = require("inquirer");
+const path = require("path");
+const fs = require("fs");
+
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+
+const render = require("./src/page-template.js");
+
+
+// TODO: Write Code to gather information about the development team members, and render the HTML file.
+
+const questions = [
+  {
+    type: 'input',
+    name: 'name',
+    message: 'What is your name?',
+  },
+  {
+    type: 'input',
+    name: 'id',
+    message: 'What is your ID?',
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'What is your email?',
+    validate: function (input) {
+      // Use a regular expression for basic email validation
+      const emailRegex = /\S+@\S+\.\S+/;
+      return emailRegex.test(input) ? true : 'Please enter a valid email address';
+    }
+  },
+  {
+    type: 'input',
+    name: 'office number',
+    message: 'What is your office number?',
+  },
+  {
+    type: 'input',
+    name: 'github',
+    message: 'Enter your GitHub Username',
+  },
+  {
+    type: 'input',
+    name: 'linkedin',
+    message: 'Enter your LinkedIn URL.',
+  },
+];
+
